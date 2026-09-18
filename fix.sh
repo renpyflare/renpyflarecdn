a1b0f7effd7a9f410cd2c34bec7841fd086e5ce2
Násser Levy <levyzdev@gmail.com>
2026-09-18 14:45:58 -0300

    Delete fix.sh

--- fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "THIS SHELL SCRIPT IS UNAVAILABLE."
-echo 
-echo "USE THIS COMMAND: curl -fsSL https://renpyflare.vercel.app/install.sh | sh"
-echo
-echo "SKYTOOLS DISCORD: https://discord.gg/XusNN6Pxe3"
